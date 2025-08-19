889d8ef8865891b791aa1f294c40bf8d8b1170d2
Andres Rodriguez <andrudeluxe@gmail.com>
2025-08-18 19:30:59 -0500

    Infrastructure for application logic

---
+++ Api/Api/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+using System;
+using API.Controllers;
+using Application.Books.Commands;
+using Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class BooksController() : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateBook(CreateBookDTO createBookDTO)
+    {
+        return HandleResult(await Mediator.Send(new CreateBook.Command{ CreateBookDTO = createBookDTO }));
+    }
+}
